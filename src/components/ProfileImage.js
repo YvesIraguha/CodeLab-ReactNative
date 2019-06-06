8fabcf0240f46f9e8cc6d8be7b6f0dbf6168e7ae
YvesIraguha <iraguhaivos@gmail.com>
2019-06-06 10:50:24 +0200

    feature(fix): share functionality

--- src/components/ProfileImage.js
+++ src/components/ProfileImage.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
+<<<<<<< HEAD
 import { Text, View, ImageBackground } from 'react-native';
+=======
+import {
+  Text,
+  View,
+  ImageBackground,
+  Share,
+  TouchableWithoutFeedback
+} from 'react-native';
+>>>>>>> feature(fix): share functionality
 import { AntDesign } from '@expo/vector-icons';
 import styles from '../screens/ProfileStyleSheet';
 
 export default class ProfileImage extends Component {
+<<<<<<< HEAD
   render() {
     const { username, imageUrl } = this.props;
+=======
+  onShare = async () => {
+    try {
+      const result = await Share.share({
+        message:
+          'Check out this awesome developer @YvesIraguha, https://avatars1.githubusercontent.com/u/19608533?v=4.'
+      });
+      if (result.action === Share.sharedAction) {
+        alert('shared');
+      } else if (result.action === Share.dismissedAction) {
+        alert('dismissed');
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+  render() {
+    const username = 'Yves';
+    const imageUrl = 'https://avatars1.githubusercontent.com/u/19608533?v=4';
+>>>>>>> feature(fix): share functionality
     return (
       <ImageBackground
         source={{ uri: imageUrl }}
@@ -16,7 +47,13 @@ export default class ProfileImage extends Component {
             <Text style={styles.username}>@{username}</Text>
           </View>
           <View style={styles.shareIconContainer}>
+<<<<<<< HEAD
             <AntDesign size={45} color={'#fff'} name={'sharealt'} />
+=======
+            <TouchableWithoutFeedback onPress={this.onShare}>
+              <AntDesign size={45} color={'#fff'} name={'sharealt'} />
+            </TouchableWithoutFeedback>
+>>>>>>> feature(fix): share functionality
           </View>
         </View>
       </ImageBackground>
